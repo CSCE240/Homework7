3b0f05950f5705e7884d02c5260553fae8267c62
ssmero <ssmero@email.sc.edu>
2016-11-17 14:59:07 -0500

    Update simulation.h

--- hw7/simulation.h
+++ hw7/simulation.h
@@ -26,23 +26,23 @@ public:
 /****************************************************************
  * Constructors and destructors for the class. 
 **/
-  Simulation();
-  virtual ~Simulation();
+ Simulation();
+ virtual ~Simulation();
 
 /****************************************************************
  * General functions.
 **/
-  void ReadPrecincts(Scanner& infile);
-  void RunSimulation(const Configuration& config,
-                     MyRandom& random, ofstream& out_stream);
-  string ToString();
-  string ToStringPcts();
+ void ReadPrecincts(Scanner& infile);
+ void RunSimulation(const Configuration& config,
+                    MyRandom& random, ofstream& out_stream);
+ string ToString();
+ string ToStringPcts();
 
 private:
 /****************************************************************
  * Variables.
 **/
-  map<int, OnePct> pcts_;
+ map<int, OnePct> pcts_;
 
 /****************************************************************
  * Private functions.
